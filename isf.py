d5e543bf472bccbb8135bb32b3cec9e3b562e44a
maliksagar96 <malik.sagar97@gmail.com>
2021-10-24 12:03:03 +0530

    Parallel ISF

--- isf.py
+++ isf.py
@@ -10,6 +10,7 @@ import trackpy as tp
 import matplotlib.pyplot as plt
 import warnings
 from warnings import warn
+import multiprocessing
 
 warnings.filterwarnings('ignore')
 
@@ -22,7 +23,7 @@ minus_one = -1
 
 j = np.sqrt(np.complex(minus_one))
 
-fname = 'link55.npy'
+fname = '/home/sagar/Documents/codes/python/s4/fiveframes.npy'
 
 inputdata = np.load(fname)   
 
@@ -53,6 +54,24 @@ def FT(q,x_t1,x_t2,y_t1,y_t2):
 	j = np.sqrt(np.complex(minus_one))
 	return 	np.sum(np.exp(-j*(q*(x_t2 + y_t2 - x_t1 - y_t1))))
 
+def calcISF(i, f0, isf):
+	
+	for ti in range(i, i+1):
+
+		ovrlapsum = 0 				#Number of slow particles
+		ft = 0 						#Fourier transform of overlap function  
+		loopCounter = 0				#For time averaging
+		for tj in range(int(inputdata[:,2][0]), frames):
+			if(ti + tj < frames):
+				ovrlap = overlapFn(b,x[:,tj],x[:,ti+tj],y[:,tj],y[:,ti+tj])
+				ovrlapsum = ovrlapsum + sum(ovrlap) 
+				ft = ft + np.real(FT(k, x[:,tj],x[:, ti+tj], y[:,tj], y[:, ti+tj]))
+				loopCounter = loopCounter + 1
+		if(loopCounter == 0):
+			break
+		isf[ti] = ft/(N * loopCounter) 
+		f0[ti] = ovrlapsum/(N * loopCounter)
+
 
 if __name__ == "__main__":
 	        
@@ -62,33 +81,26 @@ if __name__ == "__main__":
 	x = np.zeros((N, frames), dtype = float)
 	y = np.zeros((N, frames), dtype = float)
 
-	isf = np.zeros((frames, 2), dtype = complex)
-	f0 = np.zeros((frames, 2), dtype = float)
-
-	for t in range(inputdata[:,2][0], frames):
+	for t in range(int(inputdata[:,2][0]), frames):
 		frameIndex = np.where(data[:,2] == t)         # Index of the ith(or tth if you like) frame 
 		
 		datafrm = data[frameIndex]                        # Data of that frame
 		datafrm = datafrm[datafrm[:, 3].argsort()]	
-		print("Shape of datafrm = ", np.shape(datafrm))
 		x[:, t] = datafrm[:, 0]
 		y[:, t] = datafrm[:, 1]
+
+	f0 = multiprocessing.Array('d', frames)
+	isf = multiprocessing.Array('d', frames)	
 	
-	for ti in range(inputdata[:,2][0], frames):
+	processes = []
 
-			ovrlapsum = 0 				#Number of slow particles
-			ft = 0 						#Fourier transform of overlap function  
-			loopCounter = 0				#For time averaging
-			for tj in range(inputdata[:,2][0], frames):
-				if(ti + tj < frames):
-					ovrlap = overlapFn(b,x[:,tj],x[:,ti+tj],y[:,tj],y[:,ti+tj])
-					ovrlapsum = ovrlapsum + sum(ovrlap) 
-					ft = ft + np.real(FT(k, x[:,tj],x[:, ti+tj], y[:,tj], y[:, ti+tj]))
-					loopCounter = loopCounter + 1
-			isf[ti,0] = ti/fps
-			f0[ti, 0] = ti/fps
-			isf[ti, 1] = ft/(N * loopCounter) 
-			f0[ti, 1] = ovrlapsum/(N * loopCounter)
-
-	np.save('ISF'+str(conc)+'_k'+str(k), isf)
-	np.save('f0'+str(conc)+'_k'+str(k), f0)
+	for i in range(frames):	
+		p = multiprocessing.Process(target = calcISF, args = (i, f0, isf))
+		p.start()
+		processes.append(p)
+		
+	for process in processes:
+		process.join() 
+	
+	np.save('ISF'+str(conc)+str(k), isf)
+	np.save('f0'+str(conc)+str(k), f0)
